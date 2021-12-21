0965e0bb1205887c9270bee421da4f83e9df31af
Saidat Olaogun <solaogun@softalliance.co.ng>
2021-12-21 07:53:09 +0100

    initial

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name='keywords' content='ninjas' />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem Ipsun dolor sit amet consectur adipisicing elit numquam</p>
+                <p>Lorem Ipsun dolor sit amet consectur adipisicing elit numquam</p>
+            </div>
+        </>
+    );
+}
+
+export default About;
